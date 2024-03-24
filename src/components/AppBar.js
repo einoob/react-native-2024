f2212acc9fb0c3e9583f016419b2cd832a9c6ec0
Eino Lindberg <eino.lindberg@studyo.fi>
2024-03-24 17:11:05 +0100

    ex 10.7 // scrollable app bar

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -1,4 +1,4 @@
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, ScrollView } from "react-native";
 import Constants from "expo-constants";
 import theme from "../theme";
 
@@ -6,24 +6,32 @@ import AppBarTap from "./AppBarTab";
 
 const styles = StyleSheet.create({
   container: {
-    paddingTop: Constants.statusBarHeight,
+    paddingTop: Constants.statusBarHeight + 8,
     backgroundColor: theme.colors.primary,
     paddingHorizontal: 12,
-    paddingVertical: 10,
     display: "flex",
     flexDirection: "row",
+    paddingBottom: 6,
+    alignItems: "center",
   },
   text: {
     color: theme.colors.textWhite,
     fontWeight: "bold",
+    fontSize: 16,
   },
 });
 
 const AppBar = () => {
   return (
     <View style={styles.container}>
-      <AppBarTap style={styles.text} to="/">Repositories</AppBarTap>
-      <AppBarTap style={styles.text} to="/signin">Sign in</AppBarTap>
+      <ScrollView horizontal>
+        <AppBarTap style={styles.text} to="/">
+          Repositories
+        </AppBarTap>
+        <AppBarTap style={styles.text} to="/signin">
+          Sign in
+        </AppBarTap>
+      </ScrollView>
     </View>
   );
 };
