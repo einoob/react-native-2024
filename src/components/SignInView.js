b3b814db8ead11d7e2769f5138c78b4ad651e188
Eino Lindberg <eino.lindberg@studyo.fi>
2024-03-24 18:27:37 +0100

    ex 10.8 10.9 // add sign in form and form validation

--- src/components/SignInView.js
+++ src/components/SignInView.js
@@ -1,10 +1,87 @@
-import { View } from "react-native";
+import { View, TextInput, Button, StyleSheet } from "react-native";
+import { useForm, Controller } from "react-hook-form";
 import Text from "./Text";
 
+const styles = StyleSheet.create({
+  container: {
+    paddingVertical: 12,
+    paddingHorizontal: 12,
+  },
+  inputField: {
+    height: 60,
+    marginVertical: 6,
+    paddingHorizontal: 12,
+    borderColor: "#dadada",
+    borderWidth: 2,
+    borderRadius: 6,
+  },
+  borderRed: {
+    borderColor: "red",
+  },
+  errorText: {
+    color: "red"
+  }
+});
+
 const SignInView = () => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (data) => {
+    console.log(data);
+    reset();
+  };
+
   return (
-    <View>
-      <Text>The sign-in view</Text>
+    <View style={styles.container}>
+      <Controller
+        control={control}
+        rules={{
+          required: true,
+        }}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+            style={[styles.inputField, errors.username && styles.borderRed]}
+            placeholder="Username"
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+          />
+        )}
+        name="username"
+      />
+      {errors.username && <Text style={styles.errorText}>Username is required.</Text>}
+
+      <Controller
+        control={control}
+        rules={{
+          maxLength: 127,
+          required: true,
+        }}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+            secureTextEntry
+            style={styles.inputField}
+            placeholder="Password"
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+          />
+        )}
+        name="password"
+      />
+      {errors.password && <Text style={styles.errorText}>Password is required</Text>}
+
+      <Button title="Sign in" onPress={handleSubmit(onSubmit)} />
     </View>
   );
 };
